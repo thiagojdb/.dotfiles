d3669adb5e505211278851c85aa2db6ac56629b8
Thiago <thiagojdb@tutanota.com>
2023-08-29 11:58:18 +0100

    Rust install without annoying prompt

--- aptinstall.sh
+++ aptinstall.sh
@@ -44,7 +44,7 @@ install lolcat
 
 
 # Rustlang
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
 source "$HOME/.cargo/env"
 
 function install_golang {
