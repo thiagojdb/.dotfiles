8db7114c0763acb13aae91e306bc2e934c8f6c42
Thiago <thiagojdb@tutanota.com>
2023-09-01 19:50:43 +0100

    Stuff

--- aptinstall.sh
+++ aptinstall.sh
@@ -2,64 +2,60 @@
 
 sudo apt update
 
-function install {
-  which $1 &> /dev/null
+sudo apt install -y gcc pkg-config openssl libasound2-dev cmake \n
+build-essential python3 libfreetype6-dev libexpat1-dev libxcb-composite0-dev \n
+libssl-dev libx11-dev libfontconfig1-dev curl chromium-browser firefox git \n
+htop nmap openvpn tmux libfreetype6-dev pass fzf figlet lolcat fd-find \n
+ripgrep
 
-  if [ $? -ne 0 ]; then
-    echo "Installing: ${1}..."
-    sudo apt install -y $1
-  else
-    echo "Already installed: ${1}"
-  fi
-}
-
-# Basics
-install gcc
-install pkg-config
-install openssl 
-install libasound2-dev 
-install cmake 
-install build-essential 
-install python3 
-install libfreetype6-dev 
-install libexpat1-dev 
-install libxcb-composite0-dev 
-install libssl-dev 
-install libx11-dev 
-install libfontconfig1-dev
-install curl
-install chromium-browser
-install firefox
-install git
-install htop
-install nmap
-install openvpn
-install tmux
-install libfreetype6-dev
-install pass
-install fzf
-install figlet
-install lolcat
-install fd-find
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Rustlang
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
-source "$HOME/.cargo/env"
+if ! foobar_loc="$(type -p "cargo")" || [[ -z $foobar_loc ]]; then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
+	source "$HOME/.cargo/env"
+fi
 
-function install_golang {
-    goVersion=${1}
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Golang
+if ! foobar_loc="$(type -p "go")" || [[ -z $foobar_loc ]]; then
+    goVersion=$(curl "https://go.dev/VERSION?m=text" | sed -n '1p;')
     wget "https://dl.google.com/go/${goVersion}.linux-amd64.tar.gz"
     sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf ${goVersion}.linux-amd64.tar.gz
     rm ${goVersion}.linux-amd64.tar.gz
-}
+fi
 
-# Golang
-goVersion=$(curl "https://go.dev/VERSION?m=text" | sed -n '1p;')
-if ! type "go" > /dev/null; then
-    install_golang ${goVersion}
-else
-    ## TODO - logic to check if is latest version and install again
+if ! foobar_loc="$(type -p "alacritty")" || [[ -z $foobar_loc ]]; then
+    git clone https://github.com/alacritty/alacritty.git build/alacritty
+    cd build/alacritty
+    cargo build --release
+    infocmp alacritty
+    sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+    sudo cp target/release/alacritty /usr/local/bin
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+    cd ../
+    rm -rf alacritty
 fi 
+
+if ! foobar_loc="$(type -p "git-credential-manager")" || [[ -z $foobar_loc ]]; then
+    wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.3.2/gcm-linux_amd64.2.3.2.deb
+    sudo dpkg -i gcm-linux_amd64.2.3.2.deb 
+    rm gcm-linux_amd64.2.3.2.deb 
+fi 
+
+if ! foobar_loc="$(type -p "nvim")" || [[ -z $foobar_loc ]]; then
+	git clone https://github.com/neovim/neovim neovim-build
+	cd neovim-build
+	git checkout stable
+	sudo apt-get install ninja-build gettext cmake unzip curl -y
+	make CMAKE_BUILD_TYPE=Release
+	sudo make install
+	cd ../
+	rm neovim-build -rf
+fi
 
 # Fast node manager
 cargo install fnm
@@ -88,34 +84,3 @@ sdk install java 17.0.8-amzn
 sdk install java 11.0.20-amzn
 sdk install java 8.0.382-amzn
 
-
-
-
-if ! type "alacritty" > /dev/null; then
-    git clone https://github.com/alacritty/alacritty.git build/alacritty
-    cd build/alacritty
-    cargo build --release
-    infocmp alacritty
-    sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-    sudo cp target/release/alacritty /usr/local/bin
-    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-    sudo desktop-file-install extra/linux/Alacritty.desktop
-    sudo update-desktop-database
-    cd ../
-    rm -rf alacritty
-else
-    ## TODO - logic to check if is latest version and install again
-fi 
-
-if ! type "git-credential-manager" > /dev/null; then
-    wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.3.2/gcm-linux_amd64.2.3.2.deb
-    sudo dpkg -i gcm-linux_amd64.2.3.2.deb 
-    rm gcm-linux_amd64.2.3.2.deb 
-fi 
-
-if ! type "brave-browser" > /dev/null; then
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    sudo apt update
-    install brave-browser
-fi
